80f057f4319fa3fa85073cf849e764e3c9afbb67
Marcus Adriano <marcusadriano.pereira@gmail.com>
2023-05-03 11:42:23 -0300

    Set webhook

--- cmd/telegram/telegram.go
+++ cmd/telegram/telegram.go
@@ -40,6 +40,28 @@ var getWebhookInfoCmd = &cobra.Command{
 	},
 }
 
+var setWebhookConfig SetWebhookConfig
+
+var setWebhookCmd = &cobra.Command{
+	Use:   "set",
+	Short: "Set webhook details",
+	Run: func(cmd *cobra.Command, args []string) {
+
+		config := TGApiConfig{
+			URL:        telegramApiUrl,
+			Token:      token,
+			HttpClient: httpClient,
+		}
+
+		result, err := setWebhookInfo(config, setWebhookConfig)
+		if err != nil {
+			finalizeWithError(err)
+		}
+
+		fmt.Println(result)
+	},
+}
+
 var rootCmd = &cobra.Command{
 	Use:   "telegram",
 	Short: "Manage telegram webhook details",
@@ -63,7 +85,7 @@ func Execute() {
 
 func getWebhookInfo(config TGApiConfig) (string, error) {
 
-	bot, err := tgbotapi.NewBotAPIWithClient(config.Token, config.URL, &config.HttpClient)
+	bot, err := newBotWithConfig(config)
 	if err != nil {
 		return "", fmt.Errorf("error to create bot: %v", err)
 	}
@@ -77,12 +99,39 @@ func getWebhookInfo(config TGApiConfig) (string, error) {
 	return fmt.Sprintf("%s\n", webhookJson), nil
 }
 
+func setWebhookInfo(config TGApiConfig, request SetWebhookConfig) (string, error) {
+
+	bot, err := newBotWithConfig(config)
+	if err != nil {
+		return "", fmt.Errorf("error to create bot: %v", err)
+	}
+
+	resp, err := bot.MakeRequest(request.Method(), request.Params())
+	if err != nil {
+		return "", fmt.Errorf("error to set webhook: %v", err)
+	}
+
+	webhookJson, _ := json.MarshalIndent(resp, "", "  ")
+	return fmt.Sprintf("%s\n", webhookJson), nil
+}
+
+func newBotWithConfig(config TGApiConfig) (*tgbotapi.BotAPI, error) {
+	return tgbotapi.NewBotAPIWithClient(config.Token, config.URL, &config.HttpClient)
+}
+
 func initialize() {
 	webhookCmd.PersistentFlags().StringVarP(&telegramApiUrl, "telegramApiUrl", "u", tgbotapi.APIEndpoint, "Telegram API URL")
 	webhookCmd.PersistentFlags().StringVarP(&token, "token", "t", "", "Telegram bot token")
 	webhookCmd.MarkPersistentFlagRequired("token")
 
-	webhookCmd.AddCommand(getWebhookInfoCmd)
+	setWebhookCmd.PersistentFlags().StringVarP(&setWebhookConfig.URL, "webhook", "w", "", "Webhook URL")
+	setWebhookCmd.PersistentFlags().IntVarP(&setWebhookConfig.MaxConnections, "max_connections", "m", 40, "Max connections")
+	setWebhookCmd.PersistentFlags().StringArrayVarP(&setWebhookConfig.AllowedUpdates, "allowed_updates", "a", []string{}, "Allowed updates")
+	setWebhookCmd.PersistentFlags().BoolVarP(&setWebhookConfig.DropPendingUpdates, "drop_pending_updates", "d", false, "Drop pending updates")
+	setWebhookCmd.PersistentFlags().StringVarP(&setWebhookConfig.SecretToken, "secret_token", "s", "", "Secret token")
+	setWebhookCmd.MarkPersistentFlagRequired("webhook")
+
+	webhookCmd.AddCommand(getWebhookInfoCmd, setWebhookCmd)
 
 	rootCmd.AddCommand(webhookCmd)
 }
--- cmd/telegram/types.go
+++ cmd/telegram/types.go
@@ -1,9 +1,41 @@
 package telegram
 
-import "net/http"
+import (
+	"net/http"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
 
 type TGApiConfig struct {
 	URL        string
 	Token      string
 	HttpClient http.Client
+}
+
+type SetWebhookConfig struct {
+	URL                string   `json:"url"`
+	Certificate        string   `json:"certificate,omitempty"`
+	MaxConnections     int      `json:"max_connections,omitempty"`
+	AllowedUpdates     []string `json:"allowed_updates,omitempty"`
+	DropPendingUpdates bool     `json:"drop_pending_updates,omitempty"`
+	SecretToken        string   `json:"secret_token,omitempty"`
+	IpAddress          string   `json:"ip_address,omitempty"`
+}
+
+func (s SetWebhookConfig) Params() tgbotapi.Params {
+	params := make(tgbotapi.Params)
+
+	params.AddNonEmpty("url", s.URL)
+	params.AddNonEmpty("certificate", s.Certificate)
+	params.AddNonZero("max_connections", s.MaxConnections)
+	params.AddInterface("allowed_updates", s.AllowedUpdates)
+	params.AddBool("drop_pending_updates", s.DropPendingUpdates)
+	params.AddNonEmpty("secret_token", s.SecretToken)
+	params.AddNonEmpty("ip_address", s.IpAddress)
+
+	return params
+}
+
+func (s SetWebhookConfig) Method() string {
+	return "setWebhook"
 }
